2f74a0561638613c9f2ef715137ab55953775fcb
Pawel Worzala <pawel.worzala2@utlook.com>
2023-06-21 09:38:51 +0200

    auto save

--- source/i3b.js
+++ source/i3b.js
@@ -5,14 +5,29 @@ i3b
 
 */
 
+//use x32
+var numAAA = 12.23
+var numBB = 8.24
+//use x64
+var numCC = (numAAA,numBB)
+var numXX = 2.2
+var numYY = 0.0
+
 function Main(){
 
     printf('START %s',LF)
 
+    numYY = numCC * numXX
 
     printf('inner... %s',LF)
 
+    (numFF,numEE) = {numYY}/2
 
     printf('END %s',LF)
 
 }
+
+/*
+OBJ??  FUNC??  DATA??
+({TTT:({TTT},({TTT,TTT}))})
+*/
